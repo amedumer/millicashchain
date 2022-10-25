4a014dfa5281a0a57fa30d150356cfb04a00697c
Ahmet Ömer Kayabaşı <ahmetomer@sabanciuniv.edu>
2022-10-25 13:13:34 +0300

    update script

--- scripts/demo/getPrivateKeys.sh
+++ scripts/demo/getPrivateKeys.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "Private key of Alice"
 echo "y" | millicashd keys export alice --unsafe --unarmored-hex
+echo "Private key of Bob"
 echo "y" | millicashd keys export bob --unsafe --unarmored-hex
